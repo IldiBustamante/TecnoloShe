1bc30ca6406eb825194c947918ee26494f189339
IldiBustamante <49962409+IldiBustamante@users.noreply.github.com>
2019-04-24 21:54:16 -0300

    Add files via upload

---
+++ Clase2/SolucionConsoleClse2/ConsoleClse2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleClse2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Punto de inicio de programa
+
+             Console.WriteLine("Hola Mundo");
+
+            //Pausa en la consola 
+
+            Console.ReadKey();
+
+            //Definir una variable,asignar un nombre,mostrar valor en consola
+
+            //Defino variable
+
+            string strNombre;
+            int intNota; 
+
+            //Solicito valores al usuario
+
+            Console.Write("Ingrese su nombre: ");
+            strNombre = Console.ReadLine();
+            Console.Write("Ingrese su nota de \n entre 1 y 10 : "); // la \n es para un salto de linea
+            intNota = Convert.ToInt32(Console.ReadLine()); // convierte de int a string
+
+            //Muestro con la condicion si es aprobado o no, estructura de if
+
+            string strCalificacion= "Desaprobado";
+
+            if (intNota>=4)
+            {
+                //Salida cuando difiere del default
+
+                strCalificacion = "Aprobaste";
+
+            }
+
+            /* else ESTO LO USO PARA CUANDO USO EL ELSE
+            {
+               Salida cuando NO cumple condicion
+                strCalificacion = "Desaprobaste";
+            }*/ 
+
+
+            //Muestro en pantalla lo que toma de valor la variable
+
+            Console.WriteLine("Hola {0} ¿como andas? Nota {1} ",strNombre,strCalificacion);
+
+            //Pausa en la consola
+
+            Console.ReadKey();
+
+        }
+    }
+}
