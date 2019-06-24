c3a3666fd1f13d8318ae12c4538b4e72e34cf39e
IldiBustamante <49962409+IldiBustamante@users.noreply.github.com>
2019-06-24 20:36:31 -0300

    Add files via upload

---
+++ SolucionTechnoloshe/Entidades/Alumno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Entidades
+{
+      public class Alumno
+    {
+        //Aca voy a crear atributos 
+
+        private int dni;
+        private string apellido;
+        private string nombre;
+
+        //Hay propiedades explicitas y recordar que la minuscula es para privado. 
+
+        public int DNI
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        //propiedad auto implementada(atributo aplicado directamente dentro)
+
+        public string Nombre { get ; set; }
+    }
+}
---
+++ SolucionTechnoloshe/Logica/Alumno.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Logica
+{
+    public class Alumno
+    {
+        //solo incluye metodos
+
+        public void Agregar(Entidades.Alumno pAlumno)
+        {
+            // (todo)LO PONES LO QUE TENES QUE HACER llamar a metodo agregar de la capa de datos
+            //Poner quien lo creo Creado por mi y cosas asi
+
+
+        }
+            
+    }
+}
